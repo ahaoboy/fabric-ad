3f6552d06883e60ec632cba5b65ffa8c616e9695
ahaoboy <504595380@qq.com>
2020-04-04 11:36:44 +0800

    add node

--- init/init-up.sh
+++ init/init-up.sh
@@ -6,10 +6,10 @@ chmod 777 ./ -R
 ./network.sh down
 
 
-./network.sh up 
+./network.sh up -ca -s couchdb
 
 
-./network.sh createChannel
+./network.sh createChannel 
 
 ./network.sh createChannel -c channel1
 
@@ -20,27 +20,32 @@ chmod 777 ./ -R
 
 TEST_PWD=${PWD}
 
-#export TEST_PWD=${TEST_PWD}
+export TEST_PWD=${TEST_PWD}
 echo "export TEST_PWD=${TEST_PWD}" >> ~/.bashrc
 
-#export PATH=${TEST_PWD}/../bin:${TEST_PWD}:$PATH
+export PATH=${TEST_PWD}/../bin:${TEST_PWD}:$PATH
 echo "export PATH=${TEST_PWD}/../bin:${TEST_PWD}:$PATH" >> ~/.bashrc
 
 
-# export FABRIC_CFG_PATH=$TEST_PWD/../config/
+export FABRIC_CFG_PATH=$TEST_PWD/../config/
 echo "export FABRIC_CFG_PATH=$TEST_PWD/../config/" >> ~/.bashrc
 source ~/.bashrc
 
 # Environment variables for Org1
 
-# export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_TLS_ENABLED=true
 echo "export CORE_PEER_TLS_ENABLED=true" >> ~/.bashrc
 
-# export CORE_PEER_LOCALMSPID="Org1MSP"
+export CORE_PEER_LOCALMSPID="Org1MSP"
 echo "export CORE_PEER_LOCALMSPID=\"Org1MSP\"" >> ~/.bashrc
 
+export CORE_PEER_TLS_ROOTCERT_FILE=${TEST_PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 echo "export CORE_PEER_TLS_ROOTCERT_FILE=${TEST_PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" >> ~/.bashrc
+
+export CORE_PEER_MSPCONFIGPATH=${TEST_PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 echo "export CORE_PEER_MSPCONFIGPATH=${TEST_PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" >> ~/.bashrc
+
+export CORE_PEER_ADDRESS=localhost:7051
 echo "export CORE_PEER_ADDRESS=localhost:7051" >> ~/.bashrc
 
 source ~/.bashrc
@@ -53,11 +58,19 @@ peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.exa
 
 
 # Environment variables for Org2
-
+export CORE_PEER_TLS_ENABLED=true
 echo "export CORE_PEER_TLS_ENABLED=true" >> ~/.bashrc
+
+export CORE_PEER_LOCALMSPID="Org2MSP"
 echo "export CORE_PEER_LOCALMSPID=\"Org2MSP\"" >> ~/.bashrc
+
+export CORE_PEER_TLS_ROOTCERT_FILE=${TEST_PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 echo "export CORE_PEER_TLS_ROOTCERT_FILE=${TEST_PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" >> ~/.bashrc
+
+export CORE_PEER_MSPCONFIGPATH=${TEST_PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
 echo "export CORE_PEER_MSPCONFIGPATH=${TEST_PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" >> ~/.bashrc
+
+export CORE_PEER_ADDRESS=localhost:9051
 echo "export CORE_PEER_ADDRESS=localhost:9051" >> ~/.bashrc
 
 source ~/.bashrc
